5c1eda357cb3c8c6456323a1e56c82f7ca56d074
ujeon_jang <wj.jang@newploy.net>
2021-07-19 21:10:05 +0900

    HelloWorld 앱 만들기

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var uiTitle: UILabel!
+    
+    @IBAction func sayHello(_ sender: Any) {
+        self.uiTitle.text = "Hello, World!"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
